338c461e4570603c1cd00a67f611215c2c79c866
Joel Antonio <joelant97@gmail.com>
2019-08-30 14:07:36 -0400

    Falta Update Vistas

--- src/main/java/practica13/Utils/Security.java
+++ src/main/java/practica13/Utils/Security.java
@@ -9,42 +9,42 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-
-@Configurable
-@EnableGlobalMethodSecurity(securedEnabled = true)
-public class Security extends WebSecurityConfigurerAdapter {
-
-    // JPA
-    @Qualifier("usuarioService")
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/css/**", "/js/**", "/webjars/**").permitAll() // permitiendo llamadas a esas urls.
-                .antMatchers("/dbconsole/**").permitAll().antMatchers("/admin/**").hasAnyRole("ADMIN", "USER")
-                .antMatchers("/index/**").hasAnyRole("ADMIN", "USER")
-                .antMatchers("/usuarios/**").hasAnyRole("ADMIN", "USER")
-                .anyRequest().authenticated()
-                .antMatchers("/**").fullyAuthenticated()
-                .and().formLogin().loginPage("/login")
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .defaultSuccessUrl("/index")
-                .successForwardUrl("/index")
-                .failureUrl("/login?error")
-                .permitAll().and().logout().permitAll();
-
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-    }
-}
+//
+//
+//@Configurable
+//@EnableGlobalMethodSecurity(securedEnabled = true)
+//public class Security extends WebSecurityConfigurerAdapter {
+//
+//    // JPA
+//    @Qualifier("usuarioService")
+//    @Autowired
+//    private UserDetailsService userDetailsService;
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//
+//        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+//
+//        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().antMatchers("/css/**", "/js/**", "/webjars/**").permitAll() // permitiendo llamadas a esas urls.
+//                .antMatchers("/dbconsole/**").permitAll().antMatchers("/admin/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers("/index/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers("/usuarios/**").hasAnyRole("ADMIN", "USER")
+//                .anyRequest().authenticated()
+//                .antMatchers("/**").fullyAuthenticated()
+//                .and().formLogin().loginPage("/login")
+//                .usernameParameter("username")
+//                .passwordParameter("password")
+//                .defaultSuccessUrl("/index")
+//                .successForwardUrl("/index")
+//                .failureUrl("/login?error")
+//                .permitAll().and().logout().permitAll();
+//
+//        http.csrf().disable();
+//        http.headers().frameOptions().disable();
+//    }
+//}
